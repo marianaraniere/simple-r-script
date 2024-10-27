c2b81b7dd9a7f80176028518f9fb5a08b2ed3e74
marianaraniere <marianaraniere@gmail.com>
2024-10-27 15:09:20 -0400

    added code to look at the data

--- sample-script.R
+++ sample-script.R
@@ -6,12 +6,14 @@
 #
 # Last Updated: [Replace with today's date]
 #--------------------------------------------------------------
-#Set the directory
+#Set the directory -------------------------------------
 setwd('/Users/mrn29/Library/Mobile Documents/com~apple~CloudDocs/SMDM - Reproducibility course/simple-r-script')
 
 # read in the sample data -------------------------------------
 data <- read.csv('data.csv')
 
+# read in the sample data -------------------------------------
+head(data)
 
 # calculate summary statistics --------------------------------
 lapply(data, summary)
